3e7eb5709177713a9e6b2fbe0e0923dd7f59c762
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-07-08 10:22:50 -0400

    Initial commit

---
+++ controllers/usersController.js
@@ -0,0 +1,66 @@
+const usersStorage = require("../storages/usersStorage");
+const { body, validationResult } = require("express-validator");
+
+// Validation middleware
+const validateUser = [
+  body("firstName").trim().isAlpha().isLength({ min: 1, max: 10 }),
+  body("lastName").trim().isAlpha().isLength({ min: 1, max: 10 }),
+];
+
+exports.usersListGet = (req, res) => {
+  res.render("index", {
+    title: "User list",
+    users: usersStorage.getUsers(),
+  });
+};
+
+exports.usersCreateGet = (req, res) => {
+  res.render("createUser", { title: "Create user" });
+};
+
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  },
+];
+
+exports.usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user,
+  });
+};
+
+exports.usersUpdatePost = [
+  validateUser,
+  (req, res) => {
+    const user = usersStorage.getUser(req.params.id);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user,
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = req.body;
+    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    res.redirect("/");
+  },
+];
+
+exports.usersDeletePost = (req, res) => {
+  usersStorage.deleteUser(req.params.id);
+  res.redirect("/");
+};
